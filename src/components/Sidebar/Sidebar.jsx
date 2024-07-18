caed02acf02fba57cf4deb038a04c098ea49c1d1
Mahdie Ahmadi <mahdieahmadii97@gmail.com>
2024-07-19 00:07:23 +0330

    fix active route

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,49 +6,37 @@ import { BiCommentDetail } from "react-icons/bi";
 import { FiUsers } from "react-icons/fi";
 import { IoBagCheckOutline } from "react-icons/io5";
 import { BsCurrencyDollar } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
       <h1 className="sidebar-title">به داشبورد خود خوش آمدید</h1>
       <ul className="sidebar-links">
-        <li>
-          <Link to={"/"}>
-            <IoHomeOutline className="icon" />
-            صفحه اصلی
-          </Link>
-        </li>
-        <li className="active">
-          <Link to={"/products"}>
-            <MdOutlineProductionQuantityLimits className="icon" />
-            محصولات
-          </Link>
-        </li>
-        <li>
-          <Link to={"/comments"}>
-            <BiCommentDetail className="icon" />
-            کامنت‌ها
-          </Link>
-        </li>
-        <li>
-          <Link to={"/users"}>
-            <FiUsers className="icon" />
-            کاربران
-          </Link>
-        </li>
-        <li>
-          <Link to={"/orders"}>
-            <IoBagCheckOutline className="icon" />
-            سفارشات
-          </Link>
-        </li>
-        <li>
-          <Link to="/offs">
-            <BsCurrencyDollar className="icon" />
-            تخفیف‌ها
-          </Link>
-        </li>
+        <NavLink to={"/"}>
+          <IoHomeOutline className="icon" />
+          صفحه اصلی
+        </NavLink>
+        <NavLink to={"/products"}>
+          <MdOutlineProductionQuantityLimits className="icon" />
+          محصولات
+        </NavLink>
+        <NavLink to={"/comments"}>
+          <BiCommentDetail className="icon" />
+          کامنت‌ها
+        </NavLink>
+        <NavLink to={"/users"}>
+          <FiUsers className="icon" />
+          کاربران
+        </NavLink>
+        <NavLink to={"/orders"}>
+          <IoBagCheckOutline className="icon" />
+          سفارشات
+        </NavLink>
+        <NavLink to="/offs">
+          <BsCurrencyDollar className="icon" />
+          تخفیف‌ها
+        </NavLink>
       </ul>
     </div>
   );
